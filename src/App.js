8280e5200d02f565d5f04e11f9a3192951d4a571
Feleky Mirkó <feleky@gmail.com>
2018-10-16 15:18:42 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import NavBar from './components/layout/Navbar'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>MarioPlan</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <NavBar/>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
