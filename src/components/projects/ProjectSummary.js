01ba98442bb6fd5d0f258206fa6f2238e79bffb4
Feleky Mirkó <feleky@gmail.com>
2018-10-17 09:24:40 +0200

    12

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ProjectSummary = (props) =>
+const ProjectSummary = ({project}) =>
     <div className="card z-depth-0 project-summary">
         <div className='card-content grey-text text-darken-3'>
-            <span className="card-title">Project Title</span>
+            <span className="card-title">{project.title}</span>
             <p>Posted by ...</p>
             <p className="grey-text">Date ....</p>
         </div>
