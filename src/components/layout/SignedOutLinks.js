be07ef1269fd7ccf8e10a61bcf04c71370ef7505
Feleky Mirkó <feleky@gmail.com>
2018-10-18 15:06:39 +0200

    favicon

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,10 +4,10 @@ import {NavLink} from 'react-router-dom';
 const SignedOutLinks = () =>
     <ul className="right">
         <li>
-            <NavLink to='/signup'>Sing Up</NavLink>
+            <NavLink to='/signup'>Signup</NavLink>
         </li>
         <li>
-            <NavLink to='/signin'>Log In</NavLink>
+            <NavLink to='/signin'>Login</NavLink>
         </li>
     </ul>;
 
