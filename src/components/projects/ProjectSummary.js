1dae3865ee13c8e83f5b64b560136129d446149f
Feleky Mirkó <feleky@gmail.com>
2018-10-21 11:25:27 +0200

    delete try

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import moment from 'moment'
-import {firebaseConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
+import {connect} from 'react-redux'
 
-const ProjectSummary = ({project, firebase: {remove}}) =>
+import {firestoreConnect, firebaseConnect, getVal} from 'react-redux-firebase'
+
+const ProjectSummary = (props) =>
     <div className="card z-depth-0 project-summary boxShadow">
         <div className='card-content grey-text text-darken-3 '>
-            <span onClick={() => remove(`projects/${project.id}`)}>
+            <span onClick={() => props.firebaseConnect.remove(`projects/6Uk0aoRU6hOw3pMlhFdp`,console.log('siker'))}>
                 <i className="right small material-icons">delete</i>
             </span>
-            <span className="card-title">{project.title}</span>
-            <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-            <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
-            <code>{JSON.stringify(project.id, null, 4)}</code>
+            <span className="card-title">{props.project.title}</span>
+            <p>Posted by {props.project.authorFirstName} {props.project.authorLastName}</p>
+            <p className="grey-text">{moment(props.project.createdAt.toDate()).calendar()}</p>
+            {/*<code>{JSON.stringify(props, null, 4)}</code>*/}
+            {console.log(props)}
         </div>
     </div>;
 
-export default firebaseConnect()(ProjectSummary)
+const enhance = compose(
+    firestoreConnect(props => ([
+            `projects/${props.project.id}`
+        ]),
+        firebaseConnect(props => ([
+                `projects/${props.project.id}`
+            ]),
+            connect((state, props) => ({
+                    project: getVal(state.firestore.ordered.projects,
+                        `projects/${props.project.id}`),
+                })
+            )
+        )
+    )
+)
+
+export default enhance(ProjectSummary)
